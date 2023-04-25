42a5d4f01cc7b4f0c43ffea26f2a78c65249598c
Tarek H. Abdelmageed <tarekhesham9002@gmail.com>
2023-04-25 20:11:03 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace MyAudioPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::MyAudioPlayer.Properties.Resources._045;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(604, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.DimGray;
+            this.trackBar1.Location = new System.Drawing.Point(34, 76);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(562, 45);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(166, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "open";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DimGray;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(262, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 55);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Play";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DimGray;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(401, 158);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "stop";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(34, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tarek Hesham       IT";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.DimGray;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(521, 159);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "open cd";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::MyAudioPlayer.Properties.Resources._045;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button6.Location = new System.Drawing.Point(-1, 210);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(630, 21);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "video";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(5, 226);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(618, 503);
+            this.panel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button3;
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MyAudioPlayer.Properties.Resources._045;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(629, 230);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+using System.Runtime.InteropServices;
+
+
+namespace MyAudioPlayer
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string strCommand,StringBuilder strReturn, int iReturnLength,IntPtr hwndCallback);
+
+        bool mousepressing = false;
+        int MX;
+        int MY;
+        string File=null;
+        bool isplay = false;
+        bool isnew = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousepressing = true;
+            MX = e.X;
+            MY = e.Y;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MX = 0;
+            MY = 0;
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mousepressing = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mousepressing)
+            {
+                Left -= (MX - e.X);
+                Top -= (MY - e.Y);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!isplay)
+            {
+                string sCommand = null;
+                if (isnew)
+                {
+                    try
+                    {
+                        string sCommand2 = "close MediaFile notify";
+                        mciSendString(sCommand2, null, 0, this.Handle);
+                    }
+                    catch { }
+                    if (File.Substring(File.Length - 3, 3) == "wav")
+                    {
+                        sCommand = "open \"" + File + "\" type mpegvideo alias MediaFile";
+                        mciSendString(sCommand, null, 0, IntPtr.Zero);
+                        if (button6.Top > 500) 
+                        {
+                            button6.Top -= 500;
+                            Height -= 500;
+                        }
+                    }
+                    else if (File.Substring(File.Length - 3, 3) == "avi") 
+                    {
+                        sCommand = "open \"" + File + "\" type mpegvideo alias MediaFile parent " + panel1.Handle.ToInt32().ToString() + " style child";
+                        mciSendString(sCommand, null, 0, IntPtr.Zero);
+
+                        if (button6.Top < 500)
+                        {
+                            button6.Top += 500;
+                            Height += 500;
+                        }
+                    }
+                }
+                sCommand = "play MediaFile notify";
+                mciSendString(sCommand, null, 0, this.Handle);
+                isplay = true;
+                button3.Text = "Pause";
+            }
+            else
+            {
+                string sCommand = "pause MediaFile notify";
+                mciSendString(sCommand, null, 0, this.Handle);
+                isplay = false;
+                button3.Text = "Resume";
+                isnew = false;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string file = null;
+            openFileDialog1.Filter = "WAV files (*.wav)|*.wav|AVI files (*.avi)|*.avi|All files|*.*";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                file = openFileDialog1.FileName;
+            }
+            try
+            {
+                File = file;
+                label1.Text = File;
+                label1.Text += "   ";
+                stoping();
+            }
+            catch { }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            stoping();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (label1.Text.Length > 2)
+            {
+                string l = label1.Text;
+                label1.Text = label1.Text.Substring(1);
+                label1.Text += l.Substring(0, 1);
+            }
+        }
+        private void stoping() 
+        {
+            string sCommand = "seek MediaFile notify";
+            mciSendString(sCommand, null, 0, this.Handle);
+            sCommand = "stop MediaFile notify";
+            mciSendString(sCommand, null, 0, this.Handle);
+            isplay = false;
+            isnew = true;
+            button3.Text = "Play";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string sCommand = "set cdaudio door open";
+            mciSendString(sCommand, null, 127, this.Handle);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.Top < 500)
+            {
+                button6.Top += 500;
+                Height += 500;
+            }
+            else
+            {
+                button6.Top -= 500;
+                Height -= 500;
+            }
+        }
+    }
+}
